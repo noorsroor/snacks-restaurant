e4852dca2780e71a164110be9ef1e1b8cb6b6d03
noorsroor <noorsroor67@gmail.com>
2024-12-31 16:55:03 +0300

    Ex4 update

--- app3.js
+++ app3.js
@@ -1,10 +1,13 @@
 let arr=[];
 
 let username= prompt("Enter Your Name: ");
-askG();
+let gen= prompt("Enter your Gender(Female/Male): ");
+askG(gen);
 let order= prompt("Enter Your Order : shawarma, zinger, or burger");
 let ordernmae= prompt("Enter Your Order Name: "); 
 window.alert("Your order is being prepared!");
+
+let age= prompt("Enter Your Age: "); 
 
 arr.push(username);
 arr.push(gen);
@@ -17,8 +20,7 @@ for(let i=0; i<=2; i++) {
 
 
 
-function askG() {
-    let gen= prompt("Enter your Gender(Female/Male): ");
+function askG(gen) {
 if(gen =="Male") {
     window.alert("Welcome Mr "+ username);
 }else if(gen == "Female") {
@@ -26,9 +28,35 @@ if(gen =="Male") {
 }else {
     window.alert("Welcome "+ username);
 }
-
 }
 
+
+//call div
+let div=document.getElementById('container');
+
+//create elements
+let nameHtml= document.createElement('p');
+let list= document.createElement('ol');
+let genHtml= document.createElement('li');
+let ageHtml= document.createElement('li');
+let orderHtml= document.createElement('li');
+let hero= document.createElement('h3');
+
+//add content
+nameHtml.textContent = username;
+genHtml.textContent = "Gender: "+gen;
+ageHtml.textContent = "Age: "+age;
+orderHtml.textContent = "Order: "+order;
+hero.textContent = "hello this is the hero";
+
+
+//append to html
+div.appendChild(nameHtml);
+nameHtml.append(list);
+list.appendChild(genHtml);
+list.appendChild(ageHtml);
+list.appendChild(orderHtml);
+div.prepend(hero);
 
 
 
